2c4aa21de3d4885fb7352e0322f958476ca674d7
Yourun_YP <66271043+Yourun-proger@users.noreply.github.com>
2020-09-18 21:09:43 +0300

    script for functional of todolist

---
+++ script.js
@@ -0,0 +1,26 @@
+function addItem(){
+    if(document.getElementById('box').value){
+        var newItem = document.createElement("h3");
+        newItem.innerHTML = document.getElementById('box').value;
+        newItem.onclick = removeItem;
+        document.getElementById('list').appendChild(newItem);
+        saveList()
+        document.getElementById('box').value = ""
+    }
+}
+
+function removeItem(){
+    document.getElementById('list').removeChild(this)
+    saveList()
+}
+
+function saveList(){
+    localStorage.storedList = document.getElementById('list').innerHTML;
+}
+
+function loadList(){
+    document.getElementById('list').innerHTML = localStorage.storedList;
+    for(var i=0;i < list.children.length;i++){
+        list.children[i].onclick = removeItem
+    }
+}
